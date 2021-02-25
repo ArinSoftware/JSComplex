3a87e98378d54f697cf1f9aa1b163e014fe8110d
Arin <arinyazilim@gmail.com>
2021-02-25 16:32:03 +0300

    Ders 01

---
+++ ders01.js
@@ -0,0 +1,98 @@
+// VAR - LET - CONST
+
+/* let age;
+console.log(age);
+
+age = 42;
+console.log(age);
+
+const name = "Arin";
+
+console.log(name); */
+
+// name = "Elis";
+
+/* let name = "Arin";
+console.log(name);
+
+name = "Elis"; 
+console.log(name); */
+
+/* let str = "";
+
+for ( let i =0; i<10; i++) {
+    str = str.concat(i);
+    console.log(str);
+} */
+
+/* const myArr = [1, 2, 3];
+console.log(myArr);
+myArr.pop();
+console.log(myArr); */
+
+/* const student = {
+    name: "Arin",
+    age:5
+}
+
+console.log(student);
+
+Object.freeze(student);
+
+student.name = "Elis";
+console.log(student);  */
+
+/* student = {
+    name: "Elis",
+    age:5 
+}
+ */
+
+/* var age = 40;
+console.log(age);
+
+age = 42;
+console.log(age) */
+
+// VAR --> global scope / functional scope 
+
+/* var x = 1;
+console.log(x);
+
+if (x === 1 ) {
+    var x = 10;
+    console.log(x)
+}
+
+console.log(x) */
+
+// LET --> block scope
+
+/* let y = 1;
+console.log(y);
+
+if (y === 1 ) {
+    let y = 10;
+    console.log(y)
+}
+
+console.log(y) */
+
+/* const x = 10;
+console.log(window.x); */
+
+// HOISTING
+
+// var x;
+
+/* console.log(x);
+
+var x = 10;
+
+console.log(x); */
+
+// let y; XXXXX
+
+/* const y = 102;
+console.log(y);
+ */
